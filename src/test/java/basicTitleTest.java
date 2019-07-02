d1d0b284606c84e841297d03e96be0d4c6bae4b9
ADOP-EKS <zaneri.ren@gmail.com>
2019-07-02 11:35:35 +0000

    halu

--- src/test/java/basicTitleTest.java
+++ src/test/java/basicTitleTest.java
@@ -29,7 +29,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class basicTitleTest {
 	
 	//URL to visit
-	public String QWE="hello"
+	public String QWE="aaa"
 
 	//Title to check
 	public String QWE1="demo5.jx-staging.acn-atcp.com";
